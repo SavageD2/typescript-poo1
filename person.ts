5edc13c3444c18eab4453db1243725968fac174a
Marvin <bourhanemarvin@hotmail.fr>
2022-09-26 10:02:18 +0200

    I cleaned my code

--- person.ts
+++ person.ts
@@ -2,22 +2,24 @@ class Person{
     name: string;
     age: number;
 
-    constructor(PersonName:string, PersonAge:number){
-        this.name = PersonName;
-        this.age =PersonAge;
+    constructor(personName: string, personAge: number){
+        this.name = personName;
+        this.age = personAge;
     }
+
     tellMyName(){
         return "I am " + this.name;
     }
+
     tellMyAge(){
         return "and I am " + this.age+" years old";
     }   
     
 }
-let person1: Person;
-let person2: Person;
-person1 = new Person ("John", 40);
-person2 = new Person ("Mary", 35);
+
+let person1: Person = new Person ("John", 40);
+let person2: Person = new Person ("Mary", 35);
+
 
 console.log(person1.tellMyName(), person1.tellMyAge());
 console.log(person2.tellMyName(), person2.tellMyAge());
